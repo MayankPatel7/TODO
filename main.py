68c07678b7c732b60f63199d5828b63f3893ca99
Mayank Patel <91795753+MayankPatel7@users.noreply.github.com>
2024-11-09 20:02:16 +0530

    export feature
    Added export functionality to the application

--- main.py
+++ main.py
@@ -23,12 +23,21 @@ def displayTasks():
     else:
         print("The list is currently empty")
 
+def export():
+    name = input("Enter a name for todo list: ")
+    file = open("C:\\Users\\Mayank\\Downloads\\"+name+".txt" , "x")
+    for i in range(len(tasks)):
+        file.write(str(i+1)+". "+tasks[i]+"\n")
+    print("Exported list as", name+".txt")
+    file.close()
+
 while start:
     option = int(input("""
 1. Add task to list
 2. Remove task from list
 3. List all tasks
-4. Exit app
+4. Export
+5. Exit app
 Choose an option: """))
     if option == 1:
         addTask()
@@ -37,6 +46,9 @@ Choose an option: """))
     elif option == 3:
         displayTasks()
     elif option == 4:
+        export()
+    elif option == 5:
+        print("**************** THANK YOU ****************")
         start = False
     else:
         print("ENTER A VALID OPTION")
